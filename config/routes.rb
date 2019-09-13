456acef77ee8171f50c737ec262785db2c648de5
ShWareing <slwareing@icloud.com>
2019-09-12 19:47:28 -0500

    new additions

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  root "vehicles#index"
   get "vehicles" => "vehicles#index"
   get "vehicles/:id" => "vehicles#show", as: "vehicle"
-  root "vehicles#index"
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190911161827) do
+ActiveRecord::Schema.define(version: 20190912234201) do
 
   create_table "vehicles", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190911161827) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
   end
 
 end
