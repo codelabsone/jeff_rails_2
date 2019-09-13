1ba2ae054d3f096befbd6f51029b3f6d9dae535b
ShWareing <slwareing@icloud.com>
2019-09-12 20:32:39 -0500

    new additions

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -6,4 +6,29 @@ class VehiclesController < ApplicationController
     def show
         @vehicles = Vehicle.find(params[:id])
     end
+
+    def edit
+        @vehicles = Vehicle.find(params[:id])
+    end
+
+    def update
+        @vehicles = Vehicle.find(params[:id])
+        @vehicles.update(vehicle_params)
+        redirect_to @vehicles
+    end
+    
+    def new
+        @vehicles = Vehicle.new
+    end
+
+    def create
+        @vehicles = Vehicles.new(vehicle_params)
+        @vehicles.save
+        redirect_to @vehicles
+    end
+private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:name, :description, :cartype, :year, :price)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   root "vehicles#index"
-  get "vehicles" => "vehicles#index"
-  get "vehicles/:id" => "vehicles#show", as: "vehicle"
-  
+  resources :vehicles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
