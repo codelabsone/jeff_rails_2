500aa095eb5273f3ed5bc9cdce540de2c1d9d5b8
ShWareing <slwareing@icloud.com>
2019-09-30 19:21:17 -0500

    trade in

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  get "search" => 'search#show'
+  get "search" => "search#show"
 
   resources :filtrations
   root "vehicles#index"
   resources :vehicles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :trade_in
+
 end
