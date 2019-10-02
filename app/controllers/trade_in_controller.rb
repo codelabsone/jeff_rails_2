787b99954dac7e2bdae79d339219d994a9aad4df
ShWareing <slwareing@icloud.com>
2019-10-02 14:53:37 -0500

    trade in controller

---
+++ app/controllers/trade_in_controller.rb
@@ -0,0 +1,3 @@
+class TradeInController < ApplicationController
+    
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190926203719) do
+ActiveRecord::Schema.define(version: 20191001002119) do
 
   create_table "filtrations", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20190926203719) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "description"
+    t.integer "mileage"
   end
 
 end
