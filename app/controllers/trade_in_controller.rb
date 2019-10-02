2cd161bb35ef381223ae134c15196211a00d0c0d
ShWareing <slwareing@icloud.com>
2019-10-02 15:29:07 -0500

    trade in update

--- app/controllers/trade_in_controller.rb
+++ app/controllers/trade_in_controller.rb
@@ -3,5 +3,9 @@ class TradeInController < ApplicationController
     def trade_in
         @vehicles = Vehicle.find(params[:id])
     end
+
+    def index
+        @vehicles = Vehicle.all
+    end
     
 end
--- app/helpers/trade_in_helper.rb
+++ app/helpers/trade_in_helper.rb
@@ -1,3 +1,3 @@
 module TradeInHelper
-   def  
+    
 end
