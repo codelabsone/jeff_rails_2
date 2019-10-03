d0335a1144a585d552ca6bca53fcb4fee21e03cf
ShWareing <slwareing@icloud.com>
2019-10-03 18:52:51 -0500

    jeff's work

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   resources :filtrations
   root "vehicles#index"
   resources :vehicles
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
   resources :trade_in
-  get "trade_in#index"
   get "trade_in" => "trade_in#index"
 end
