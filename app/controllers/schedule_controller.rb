ceac614c92059ee59e2c383d0e8bcab9a172923e
ShWareing <slwareing@icloud.com>
2019-10-10 18:33:37 -0500

    stuff

--- app/controllers/schedule_controller.rb
+++ app/controllers/schedule_controller.rb
@@ -3,4 +3,7 @@ class ScheduleController < ApplicationController
     def new
     end
 
+    def index 
+        render "schedule"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'tune/up'
+
+  resources :schedule
+  get "schedule" => "schedule#index"
 
   get "search" => "search#show"
 
